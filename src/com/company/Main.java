a558ee6c10d4854651cb226b9b94c78966686834
Betelihem Kabtiymer <bkabtiymer@gmail.com>
2019-09-10 15:49:53 -0400

    Presidents

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main{
+
+    public static void main(String[] args){
+        int presidents=10;
+        String userInp="";
+
+   String[] presiName= new String[presidents];
+        Scanner input= new Scanner(System.in);
+
+    System.out.println("Enter the last 10 presidents: ");
+   for(int i=0;i<presiName.length;i++){
+       System.out.println("Enter a president: ");
+       presiName[i]=input.nextLine();
+//
+       System.out.println("Type history to quit or any other key to Continue");
+       userInp=input.nextLine();
+       if(userInp.equalsIgnoreCase("history")){
+           System.out.println("Thanks bye");
+           break;
+       }
+   }
+    for(String president: presiName){
+        System.out.println("Here are the presidents you entered:"+president);
+    }
+
+
+    }
+}
